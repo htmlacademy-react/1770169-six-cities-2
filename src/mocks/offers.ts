2e4ba17eb1ac8fef8b1507f089112a7a7027798b
kaplya-sys <strim.sv2010@gmail.com>
2024-02-23 19:58:41 +0500

    added four poppy data for Amsterdam

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -192,5 +192,205 @@ export const offers: Offers = [
     rating: 3.8,
     bedrooms: 3,
     maxAdults: 5
+  },
+  {
+    id: 'fe56d16a-1cd2-4916-934a-cabf8ae3dcb7',
+    title: 'Waterfront with extraordinary view',
+    description: 'Peaceful studio in the most wanted area in town. Quiet house Near of everything. Completely renovated. Lovely neighbourhood, lot of trendy shops, restaurants and bars in a walking distance.',
+    type: 'room',
+    price: 247,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/9.jpg',
+    images: [
+      'https://13.design.htmlacademy.pro/static/hotel/3.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/18.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/13.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/10.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/9.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/15.jpg'
+    ],
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3609553943508,
+      longitude: 54.85309666406198,
+      zoom: 16
+    },
+    goods: [
+      'Washer',
+      'Washing machine',
+      'Heating',
+      'Kitchen',
+      'Air conditioning',
+      'Fridge'
+    ],
+    host: {
+      isPro: true,
+      name: 'Angelina',
+      avatarUrl: 'https://13.design.htmlacademy.pro/static/host/avatar-angelina.jpg'
+    },
+    isPremium: false,
+    isFavorite: false,
+    rating: 1.1,
+    bedrooms: 1,
+    maxAdults: 1
+  },
+  {
+    id: '1d66c063-38ca-43fc-92ce-93f570f7eb2c',
+    title: 'The Pondhouse - A Magical Place',
+    description: 'Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.',
+    type: 'apartment',
+    price: 313,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/10.jpg',
+    images: [
+      'https://13.design.htmlacademy.pro/static/hotel/8.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/12.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/2.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/20.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/16.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/9.jpg'
+    ],
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3609553943508,
+      longitude: 4.85309666406198,
+      zoom: 16
+    },
+    goods: [
+      'Breakfast',
+      'Heating',
+      'Wi-Fi',
+      'Baby seat',
+      'Washer',
+      'Cable TV',
+      'Dishwasher',
+      'Coffee machine',
+      'Laptop friendly workspace'
+    ],
+    host: {
+      isPro: true,
+      name: 'Angelina',
+      avatarUrl: 'https://13.design.htmlacademy.pro/static/host/avatar-angelina.jpg'
+    },
+    isPremium: false,
+    isFavorite: false,
+    rating: 4.3,
+    bedrooms: 4,
+    maxAdults: 9
+  },
+  {
+    id: '5756c679-e7d0-4812-9be8-53f5234181c4',
+    title: 'House in countryside',
+    description: 'I am happy to welcome you to my apartment in the city center! Three words: location, cosy and chic!',
+    type: 'apartment',
+    price: 358,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/3.jpg',
+    images: [
+      'https://13.design.htmlacademy.pro/static/hotel/3.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/5.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/20.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/13.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/6.jpg'
+    ],
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
+      zoom: 16
+    },
+    goods: [
+      'Coffee machine',
+      'Laptop friendly workspace',
+      'Fridge',
+      'Washing machine',
+      'Baby seat',
+      'Cable TV',
+      'Towels',
+      'Dishwasher',
+      'Wi-Fi',
+      'Washer',
+      'Air conditioning',
+      'Breakfast'
+    ],
+    host: {
+      isPro: true,
+      name: 'Angelina',
+      avatarUrl: 'https://13.design.htmlacademy.pro/static/host/avatar-angelina.jpg'
+    },
+    isPremium: false,
+    isFavorite: false,
+    rating: 3.4,
+    bedrooms: 3,
+    maxAdults: 4
+  },
+  {
+    id: 'fe1a8ce0-3fdf-4f8c-b030-85b9611b4719',
+    title: 'The Pondhouse - A Magical Place',
+    description: 'I rent out a very sunny and bright apartment only 7 minutes walking distance to the metro station. The apartment has a spacious living room with a kitchen, one bedroom and a bathroom with mit bath. A terrace can be used in summer.',
+    type: 'hotel',
+    price: 424,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/20.jpg',
+    images: [
+      'https://13.design.htmlacademy.pro/static/hotel/13.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/18.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/2.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/11.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/17.jpg',
+      'https://13.design.htmlacademy.pro/static/hotel/14.jpg'
+    ],
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    goods: [
+      'Heating',
+      'Fridge',
+      'Washer',
+      'Breakfast',
+      'Dishwasher',
+      'Coffee machine',
+      'Wi-Fi',
+      'Laptop friendly workspace',
+      'Baby seat'
+    ],
+    host: {
+      isPro: true,
+      name: 'Angelina',
+      avatarUrl: 'https://13.design.htmlacademy.pro/static/host/avatar-angelina.jpg'
+    },
+    isPremium: true,
+    isFavorite: false,
+    rating: 2.8,
+    bedrooms: 1,
+    maxAdults: 1
   }
 ];
