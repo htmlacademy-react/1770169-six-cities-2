2b096183333f484d6bd31df80af7fe06c973af0d
kaplya-sys <strim.sv2010@gmail.com>
2024-03-06 00:57:04 +0500

    remove mocks data

--- src/mocks/user.ts
+++
@@ -1,9 +0,0 @@
-import {FullUser} from '../types/user-type';
-
-export const user: FullUser = {
-  name: 'Oliver Conner',
-  avatarUrl: 'https://url-to-image/image.png',
-  isPro: false,
-  email: 'Oliver.conner@gmail.com',
-  token: 'T2xpdmVyLmNvbm5lckBnbWFpbC5jb20='
-};
