911bf83a32dab0a533e3354ce0952885c73d0b6b
kaplya-sys <strim.sv2010@gmail.com>
2024-03-09 18:58:43 +0500

    addde sort offers and change sort comments

--- src/utils/sort-utils.ts
+++ src/utils/sort-utils.ts
@@ -1,20 +1,26 @@
 import * as dayjs from 'dayjs';
 
+import {sortTypes} from '../const';
 import {Comment} from '../types/comment-type';
-import {Offer} from '../types/offer-type';
+import {Offer, Offers} from '../types/offer-type';
 
 
 const sortCommentsByDate = (a: Comment, b: Comment): number => dayjs(b.date).diff(dayjs(a.date));
 
-const sortOffersByAscendingPrice = (a: Offer, b: Offer): number => b.price - a.price;
+const sortOffersByAscendingPrice = (a: Offer, b: Offer): number => a.price - b.price;
 
-const sortOffersByDescendingPrice = (a: Offer, b: Offer): number => a.price - b.price;
+const sortOffersByDescendingPrice = (a: Offer, b: Offer): number => b.price - a.price;
 
 const sortOffersByRating = (a: Offer, b: Offer): number => b.rating - a.rating;
 
+const sort = {
+  [sortTypes[0].name]: (offers: Offers) => offers,
+  [sortTypes[1].name]: (offers: Offers) => [...offers].sort(sortOffersByAscendingPrice),
+  [sortTypes[2].name]: (offers: Offers) => [...offers].sort(sortOffersByDescendingPrice),
+  [sortTypes[3].name]: (offers: Offers) => [...offers].sort(sortOffersByRating),
+};
+
 export {
   sortCommentsByDate,
-  sortOffersByAscendingPrice,
-  sortOffersByDescendingPrice,
-  sortOffersByRating
+  sort
 };
