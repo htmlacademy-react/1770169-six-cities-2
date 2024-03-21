c6b45ae04186d792c38e66af113da7fe83c12f10
kaplya-sys <strim.sv2010@gmail.com>
2024-03-21 23:27:23 +0500

    added sort comments and empty favorites

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -4,11 +4,9 @@ import {Helmet} from 'react-helmet-async';
 import Layout from '../../components/layout/layout';
 import PlaceList from '../../components/place-list/place-list';
 import {useAppSelector} from '../../hooks/use-store';
-import {selectAuthorizationStatus} from '../../store/user/user.selector';
 import {selectFavoriteOffers} from '../../store/favoriteOffers/favoriteOffers.selector';
 
 const FavoritesPage = () => {
-  const authorizationStatus = useAppSelector(selectAuthorizationStatus);
   const favoriteOffers = useAppSelector(selectFavoriteOffers);
   const favoriteCities = new Set(favoriteOffers.map((favorite) => favorite.city.name));
 
@@ -27,30 +25,39 @@ const FavoritesPage = () => {
         <title>6 cities | Saved listing</title>
       </Helmet>
       <div className="page__favorites-container container">
-        <section className="favorites">
-          <h1 className="favorites__title">Saved listing</h1>
-          <ul className="favorites__list">
-            {[...favoriteCities].map((city) => (
-              <li className="favorites__locations-items" key={city}>
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="#">
-                      <span>{city}</span>
-                    </a>
+        {favoriteOffers.length ?
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <ul className="favorites__list">
+              {[...favoriteCities].map((city) => (
+                <li className="favorites__locations-items" key={city}>
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <a className="locations__item-link" href="#">
+                        <span>{city}</span>
+                      </a>
+                    </div>
                   </div>
-                </div>
-                <PlaceList
-                  offers={favoriteOffers.filter((favorite) => favorite.city.name === city)}
-                  authorizationStatus={authorizationStatus}
-                  placeCardClassName='favorites__card place-card'
-                  imageWrapperClassName='favorites__image-wrapper place-card__image-wrapper'
-                  cardInfoClassName='favorites__card-info place-card__info'
-                  listClassName='favorites__places'
-                />
-              </li>
-            ))}
-          </ul>
-        </section>
+                  <PlaceList
+                    offers={favoriteOffers.filter((favorite) => favorite.city.name === city)}
+                    placeCardClassName='favorites__card place-card'
+                    imageWrapperClassName='favorites__image-wrapper place-card__image-wrapper'
+                    cardInfoClassName='favorites__card-info place-card__info'
+                    listClassName='favorites__places'
+                  />
+                </li>
+              ))}
+            </ul>
+          </section> :
+          <section className="favorites favorites--empty">
+            <h1 className="visually-hidden">Favorites (empty)</h1>
+            <div className="favorites__status-wrapper">
+              <b className="favorites__status">Nothing yet saved.</b>
+              <p className="favorites__status-description">
+                Save properties to narrow down search or plan your future trips.
+              </p>
+            </div>
+          </section>}
       </div>
     </Layout>
   );
